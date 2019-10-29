972c08de49491ab289bd671f82a3f1f9acf4caf7
JEFFXieYuZhi <1650029324@qq.com>
2019-10-29 17:06:49 +0800

    用HBuilderX开发的微信小程序塑健，完成登录页面和TabBar

---
+++ App.vue
@@ -0,0 +1,33 @@
+<script>
+	// APP.vue文件
+	export default {
+		onLaunch: function() {//
+			console.log('App Launch')
+		},
+		onShow: function() {
+			console.log('App Show')
+		},
+		onHide: function() {
+			console.log('App Hide')
+		}
+	}
+</script>
+
+<style>
+	/*浏览器初始化的默认样式*/
+	@import "static/css/initialize.css";
+
+	/*每个页面公共css */
+	* {
+		font-size: 16px;
+	}
+
+	/*解决图片因为高度而不能铺满的问题*/
+	uni-page-body {
+		height: 100%;
+	}
+
+	page {
+		height: 100%;
+	}
+</style>
---
+++ pages/components/home/home.vue
@@ -0,0 +1,14 @@
+<template>
+	<view>
+		<h1>微信小程序首页</h1>
+	</view>
+</template>
+
+<script>
+</script>
+
+<style lang='less'>
+	h1{
+		color:blue
+	}
+</style>
